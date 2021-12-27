4ad9b5c301deb0a46d14b9acd977870bda88436c
Glauber Santiago Rocha <rglaubersantiago@gmail.com>
2021-12-27 11:47:28 -0300

    Estilizando componentes

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota";
+import CardNota from "./CardNota/CardNota";
 
 class ListaDeNotas extends Component {
   render() {
-    return <ul>{Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+    return <ul>{Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
       return(
-        <li>
-          <div>{categoria}</div>
+        <li key={index}>
           <CardNota />
         </li>
       );
